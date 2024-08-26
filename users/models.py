6900545a9eaaf14e84cf1b1c5af3ba9f551b7aff
edemura <demura.egor@gmail.com>
2024-08-26 16:11:04 +0300

    added prev robo7 logic #renamed menu items

--- users/models.py
+++ users/models.py
@@ -96,8 +96,8 @@ class Location(CreateTracker):
 class Robo7Task(models.Model):
   
   class Meta:
-     verbose_name=_('Задание на приборе')
-     verbose_name_plural=_('Задания на приборе')
+     verbose_name=_('Задание для BC ROBO7')
+     verbose_name_plural=_('Задания для BC ROBO7')
 
   patient_fio = models.CharField(max_length=255, verbose_name="ФИО пациента")
   analysis = models.CharField(max_length=255, verbose_name="Наименование вида исследования")
@@ -125,8 +125,8 @@ class Robo7Task(models.Model):
 class Incomejson(models.Model):
 
     class Meta:
-        verbose_name=_('Входящие JSON')
-        verbose_name_plural=_('Входящие JSON')
+        verbose_name=_('Входящие JSON тест')
+        verbose_name_plural=_('Входящие JSON тест')
 
     create_datetime = models.DateTimeField(null=True, auto_now=False, auto_now_add=True, verbose_name='Создано')
     is_processed = models.BooleanField(null=True, default=None, verbose_name="Валидация пройдена")
@@ -151,8 +151,8 @@ class Incomejson(models.Model):
 class TaskJson(models.Model):
 
     class Meta:
-        verbose_name=_('Входящие JSON таск')
-        verbose_name_plural=_('Входящие JSON таск')
+        verbose_name=_('Входящие JSON')
+        verbose_name_plural=_('Входящие JSON')
 
     create_datetime = models.DateTimeField(null=True, auto_now=False, auto_now_add=True, verbose_name='Создано')
     is_task_set = models.BooleanField(null=True, default=None, verbose_name="создан таск")
