da00b86e1b1a5233397c8748ae1f4dd560cc7981
edemura <demura.egor@gmail.com>
2024-09-16 14:10:50 +0300

    fix #5 queue calc func - queue num save as 6 for test

--- users/tasks.py
+++ users/tasks.py
@@ -97,8 +97,8 @@ def tray_assign():
                 i.tray_num=tray_tube.tray
                 i.is_tray_assigned=True
                 i.is_validated=True
-                #i.queue_num=i.queue_calculate()
-                i.queue_num=5
+                i.queue_num=i.queue_calculate()
+                
             else:
                 i.is_tray_assigned=False
                 i.exception_text=f"Для вида исследования {i.analysis}, не найдено доступных видов пробирок"
