4274b327bcd3beee8b42ea983cf87f9023dbbc7b
edemura <demura.egor@gmail.com>
2024-09-18 15:54:14 +0300

    fix #6 queue calc func - save & fix self

--- users/models.py
+++ users/models.py
@@ -126,9 +126,10 @@ class Robo7Task(models.Model):
      
      spk=self.pk
      
-     
-     if self.get_previous_by_create_datetime(pk__lt=spk).exists():
-        prev=self.get_previous_by_create_datetime(pk__lt=spk)
+     #get_previous_by_
+     #get_previous_by_create_datetime(pk__lt=spk)
+     if self.get_previous_by_id().exists():
+        prev=self.get_previous_by_id()
         if self.create_datetime.date()==prev.create_datetime.date():
             self.queue_num=prev.queue_num+1
         else: 
